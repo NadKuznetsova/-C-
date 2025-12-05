0b81a817c4bdcfafe112ef5abb774bfee93cc5af
NadKuznetsova <nadya.kuznetsova.2007@mail.ru>
2025-12-05 09:31:35 +0300

    Написала основной код

---
+++ task17/task17/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 2, 4, 6, 8, 10 };
+        bool allEvent = true;
+        foreach (int number in numbers)
+        {
+            if (number % 2 != 0)
+            {
+                allEvent = false;
+                break;
+            }
+        }
+        Console.WriteLine(allEvent);
+    }
+}
